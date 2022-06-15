02f1b52e279c9ccf68830b76ba748c4ab0ae52e2
marciliodevjava <marciliodevjava@gmail.com>
2022-06-15 08:20:44 -0300

    Conceitos Básico

---
+++ Seção 1 -  Configurando o Ambiente no Windows  e no Linux/Aula 3/index.js
@@ -0,0 +1,5 @@
+                                //Diferenças entre "" - '' - ``
+
+console.log('"Marcilio Zanatta"');// Serve para fechar e exibir as aspas.
+console.log("'Marcílio Zanatta'");// Serve para fechar e exibir as aspas.
+console.log(`Marcilio Zanatta`);// Utilizado para fazer templates stream.
