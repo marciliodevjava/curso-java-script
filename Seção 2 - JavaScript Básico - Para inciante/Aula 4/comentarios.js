27ab1cdb518d00739aafbe4be6f48c798b584d3c
marciliodevjava <marciliodevjava@gmail.com>
2022-06-15 09:03:07 -0300

    Curso de JavaScript - Básico para inciante

---
+++ Seção 2 - JavaScript Básico - Para inciante/Aula 4/comentarios.js
@@ -0,0 +1,23 @@
+// Para escrever comentários utiliza-se "//
+
+console.log('Olá mundo!'); // Aqui tem um método console.log();
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+//console.log('Linha 5');// Código comentado
+
+/* Comentário de bloco
+console.log('Linha 6');
+console.log('Linha 7');
+console.log('Linha 8');
+*/
+
+// Vai ser ignorado essa linha pois é um comentário
+
+/*
+* Um título
+*
+* Lorem ipsum
+*/
