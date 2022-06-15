f4f82b85cd80b562abb6ef6d0a4b9297341cb3e4
marciliodevjava <marciliodevjava@gmail.com>
2022-06-15 08:51:06 -0300

    Curso de JavaScript - Básico para iniciante

---
+++ Secão 2 - JavaScript Básico - Para inciante/Aula 3/exercicios/index.js
@@ -0,0 +1,2 @@
+// Exercicios - Aula 3
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript as', 08, 'da manhã');
---
+++ Secão 2 - JavaScript Básico - Para inciante/Aula 4/comentarios.js
@@ -0,0 +1,9 @@
+// Para escrever comentários utiliza-se "//
+
+console.log('Olá mundo!'); // Aqui tem um método console.log();
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+// Vai ser ignorado essa linha pois é um comentário
