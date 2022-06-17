3721ad2934c00443b867f850acf0458b7a4739b0
marciliodevjava <marciliodevjava@gmail.com>
2022-06-17 09:13:23 -0300

    Curso de JavaScript - Conceitos Básicos

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 6/index.js
@@ -0,0 +1,16 @@
+let nome = "João";// String 
+
+console.log(nome, "nasceu em 1984");
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
+
+// Variavel inicalizada com valor vazio - undefind
+
+let nome1;
+nome1 = "Pedro";// Aqui ela deixa de ser undefind pois foi declarada
+console.log(nome1);
+
+nome1 = "Otávio";
+console.log(nome1);
