93269ed9249807422dc86400603b2c021fb0a914
marciliodevjava <marciliodevjava@gmail.com>
2022-06-22 09:48:04 -0300

    Curso Básico de JavaScript

--- Seção 2 - JavaScript Básico - Para inciante/aula 13/aula13.js
+++ Seção 2 - JavaScript Básico - Para inciante/aula 13/aula13.js
@@ -1,6 +1,6 @@
 // 0123457
 
-let umaString = 'Um texto';
+let umaString = "Um texto";
 
 console.log(umaString.charAt(8));
 console.log(umaString.concat(" em um lindo dia."));
@@ -9,6 +9,11 @@ console.log(`${umaString} em um lindo dia.`);
 
 console.log(umaString.indexOf("Um"));
 console.log(umaString.lastIndexOf("t"));
-console.log(umaString.replace("texto","TEXTO"));
-console.log(umaString.length);// Retorna exatamente o tamanho.
-console.log(umaString.slice(2,9));
+console.log(umaString.replace("texto", "TEXTO"));
+console.log(umaString.length); // Retorna exatamente o tamanho.
+console.log(umaString.slice(2, 9));
+
+let stringNew = "O rato roeu a roupa do rei de roma";
+
+console.log(stringNew.toUpperCase());
+console.log(stringNew.toLowerCase());
