a47d342fc23ec5944e33d4edc2e431eabcb9fbb8
marciliodevjava <marciliodevjava@gmail.com>
2022-06-24 08:32:57 -0300

    Curso Básico de JavaScript

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 16/assets/js/index.js
@@ -0,0 +1,12 @@
+const numero = Number(prompt("Digite um número: "));
+const numeroTitulo = document.getElementById("numero-titulo");
+const texto = document.getElementById("texto");
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: <strong>${numero ** 0.5}</strong>.</p>`;
+texto.innerHTML += `<p><strong>${numero}</strong>, é inteiro: ${Number.isInteger(numero)}</strong>.</p>`;
+texto.innerHTML += `<p>É um NaN: <strong>${Number.isNaN(numero)}</strong>.</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: <strong>${Math.floor(numero)}</strong>.</p>`;
+texto.innerHTML += `<p>Arredondando para cima: <strong>${Math.ceil(numero)}</strong>.</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: <strong>${numero.toFixed(2)}</strong>.</p>`;
