7ce450e4c44f55f94db46547149bb682918b9fd2
marciliodevjava <marciliodevjava@gmail.com>
2022-06-27 10:17:11 -0300

    Curso Básico de JavaScript

--- Seção 2 - JavaScript Básico - Para inciante/aula 20/assets/js/script.js
+++ Seção 2 - JavaScript Básico - Para inciante/aula 20/assets/js/script.js
@@ -1,1 +1,14 @@
-const nome = 'Luiz';
+// IIFE
+function meuEscopo() {
+  const form = document.querySelector(".form");
+
+  /*  form.onsubmit = function (evento) {
+      evento.preventDefault();
+      alert("Formulário enviado com sucesso!");
+      console.log("Formulário enviado com sucesso!");
+  }; 
+*/
+  form.addEventListener();
+}
+
+meuEscopo();
