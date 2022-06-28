a1898090b809709b968d8f8c171760ea36175483
marciliodevjava <marciliodevjava@gmail.com>
2022-06-28 09:18:24 -0300

    Curso Básico de JavaScript

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 21/index.js
@@ -0,0 +1,23 @@
+/**
+ * Operadores de comparação
+ * > Maior que
+ * >= Maior que ou Igual
+ * < Menor
+ * <= Menor que ou Igual
+ * == Igualdade (valor)
+ * === Igualdade Estrito (valor e tipo)
+ * != Dideferente (valor)
+ * !== Diferente Estrito (valor e tipo)
+ */
+
+// Maior que
+const n1 = 10;
+const n2 = 5;
+console.log(`${n1} > ${n2}, Maior que:`, n1 > n2);
+console.log(`${n1} >= ${n2}, Maior que ou igual:`, n1 >= n2);
+console.log(`${n1} < ${n2}, Menor que:`, n1 < n2);
+console.log(`${n1} <= ${n2}, Menor que ou igual:`, n1 <= n2);
+console.log(`${n1} == ${n2}, Igualdade (valor):`, n1 == n2);
+console.log(`${n1} === ${n2}, Igualdade Expressiva (valor e tipo):`, n1 === n2);
+console.log(`${n1} != ${n2}, Diferente (valor):`, n1 != n2);
+console.log(`${n1} !== ${n2}, Diferente Expressiva (valor e tipo)`, n1 !== n2);
---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 22/index.js
@@ -0,0 +1,10 @@
+/*
+Operadores Lógicos
+&& - AND - E
+|| - OR - OU
+! - NOT - Não
+*/
+
+console.log("AND", true && true);
+console.log("OR:", true || false);
+console.log("NOT:", !false);
