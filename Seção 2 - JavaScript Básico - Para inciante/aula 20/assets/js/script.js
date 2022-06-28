feac9717fdff4487866019b93c28dcf03a326c0c
marciliodevjava <marciliodevjava@gmail.com>
2022-06-28 08:36:27 -0300

    Curso Básico de JavaScript

--- Seção 2 - JavaScript Básico - Para inciante/aula 20/assets/js/script.js
+++ Seção 2 - JavaScript Básico - Para inciante/aula 20/assets/js/script.js
@@ -1,14 +1,44 @@
 // IIFE
 function meuEscopo() {
   const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
 
+  const pessoas = [];
   /*  form.onsubmit = function (evento) {
       evento.preventDefault();
       alert("Formulário enviado com sucesso!");
       console.log("Formulário enviado com sucesso!");
   }; 
-*/
-  form.addEventListener();
+  */
+  /*
+ let contador =1;
+  function recebeEventoForm(evento) {
+      evento.preventDefault();
+      console.log(`Form não foi enviado, ${contador} tentativa.`);
+      contador++;
+  }
+  */
+
+  function recebeEventoForm(evento) {
+    evento.preventDefault();
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
+
+    pessoas.push = {
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    };
+
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>Nome: ${nome.value},  Sobrenome: ${sobrenome.value},  Peso: ${peso.value},  Altura: ${altura.value} .</p>`;
+  }
+
+  form.addEventListener("submit", recebeEventoForm);
 }
 
 meuEscopo();
