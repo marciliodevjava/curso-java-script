e29458d5634cfd666654c7a59a13e9d741ec7752
marciliodevjava <marciliodevjava@gmail.com>
2022-06-29 15:33:24 -0300

    Curso Básico de JavaScript

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 25/index.js
@@ -0,0 +1,13 @@
+const numero = 10;
+
+// Se isso ocorrer
+
+if (numero >= 0 && numero <= 5) {
+  console.log(`Sim, numero: ${numero} é maior que 0 e menor igual à 5!`);
+} else if (numero > 5 && numero <= 10) {
+  console.log(`Sim, numero: ${numero} é maior que 5 e menor igual à 10!`);
+} else if (numero > 10 && numero <= 15) {
+  console.log(`Sim, numero: ${numero} é maior que 5 e menor igual à 10!`);
+} else if (numero > 15 && numero <= 20) {
+  console.log(`Sim, numero: ${numero} é maior que 5 e menor igual à 10!`);
+}
