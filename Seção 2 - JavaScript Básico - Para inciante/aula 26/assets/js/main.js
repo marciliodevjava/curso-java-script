24956ef12eb3c12a0d660260db2b33534eff028d
marciliodevjava <marciliodevjava@gmail.com>
2022-07-04 07:31:54 -0300

    Curso Básico de JavaScript

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 26/assets/js/main.js
@@ -0,0 +1,17 @@
+// Capturar evento de submit do formulário
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Evento previnido!");
+  setResultado("Olá mundo");
+});
+
+function setResultado(msg) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+  const p = document.createElement("p");
+  p.classList.add("paragrafo-resultado");
+  p.innerHTML = "Qualquer coisa";
+  resultado.appendChild(p);
+}
