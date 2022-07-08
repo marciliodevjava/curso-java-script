7452d7a5f6b7c14bbdcd3f1e57466c29cc28b48b
marciliodevjava <marciliodevjava@gmail.com>
2022-07-08 15:21:58 -0300

    Curso Básico de JavaScript

---
+++ Seção 2 - JavaScript Básico - Para inciante/aula 34/index.js
@@ -0,0 +1,10 @@
+for(let i = 1; i < 11; i++){
+    const par = i % 2 == 0 ? 'Par': 'Ímpa';
+    console.log(`Linha ${i}, número é ${par}.`)
+}
+
+const frutas = ['Marça', 'Pêra', 'Uva','Melância','Kiwi'];
+for (let i = 0; i < frutas.length; i++) {
+    const fruta = frutas[i];
+    console.log(`Fruta: ${fruta}`);
+}
